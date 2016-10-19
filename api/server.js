e0bcb577d3cb0ed9324291706540217332c66aca
Luka Lopusina <lopusina.luka@gmail.com>
2016-10-19 11:23:15 +0200

    Init commit

---
+++ api/server.js
@@ -0,0 +1,48 @@
+'use strict'
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+let goodOptions = {
+    reporters: {
+        console: [
+            {
+                module: 'good-console',
+                args: [{log: '*', response: '*'}]
+            },
+            'stdout'
+        ]
+    }
+};
+
+server.register([
+    {
+        register: require('inert')
+    },
+    {
+        register: require('good'),
+        options: goodOptions
+    }
+], err => {
+
+    // Client
+    server.route({
+        method: 'GET',
+        path: '/{path*}',
+        handler: {
+            directory: {
+                path: './public',
+                listing: false,
+                index: true
+            }
+        }
+    });
+
+    server.start(() => console.log(`Started at: ${server.info.uri}`));
+
+});
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Hapi.js Template
+
+## Prerequisites
+
+Install **nodemon** module in order to automaticaly restart server when something is changed.
+
+```
+npm install -g nodemon
+```
+
+## Start app
+
+Clone repository:
+```
+git clone https://github.com/lukalopusina/hapi-template.git
+cd hapi-template
+```
+
+Install dependencies:
+```
+npm install
+```
+
+
+Start server:
+
+```
+npm run develop
+```
+
+Application url: http://localhost:8000/
